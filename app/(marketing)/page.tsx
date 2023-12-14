e8d720139027a935f58c62af3969a13a1395916e
nasrullah <102759013+itsNas@users.noreply.github.com>
2023-12-14 08:20:01 +0800

    setup folder and import img

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const MarketingPage = () => {
+  return (
+    <div>
+      <Button>marketing</Button>
+    </div>
+  );
+};
+
+export default MarketingPage;
